deb64c60b192d182b66787cb40605b086ad11012
Victor L. Senior <publicdomainvic@gmail.com>
2020-07-22 16:51:18 -0400

    Add OnAboutToolStripMenuItemClick() code

--- ConsolidateDirectory/MainForm.cs
+++ ConsolidateDirectory/MainForm.cs
@@ -11,8 +11,10 @@ namespace ConsolidateDirectory
     using System.Diagnostics;
     using System.Drawing;
     using System.IO;
+    using System.Reflection;
     using System.Windows.Forms;
     using Microsoft.Win32;
+    using PublicDomain;
 
     /// <summary>
     /// Description of MainForm.
@@ -23,6 +25,11 @@ namespace ConsolidateDirectory
         /// The name of the icon file.
         /// </summary>
         private string iconFileName = "cd-menu-icon.ico";
+
+        /// <summary>
+        /// The associated icon.
+        /// </summary>
+        private Icon associatedIcon = null;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="T:ConsolidateDirectory.MainForm"/> class.
@@ -191,7 +198,45 @@ namespace ConsolidateDirectory
         /// <param name="e">Event arguments.</param>
         private void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Set license text
+            var licenseText = $"CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication{Environment.NewLine}" +
+                $"https://creativecommons.org/publicdomain/zero/1.0/legalcode{Environment.NewLine}{Environment.NewLine}" +
+                $"Libraries and icons have separate licenses.{Environment.NewLine}{Environment.NewLine}" +
+                $"Folder icon by LoveYouAll - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/illustrations/folder-icon-web-internet-web-icons-2389217/{Environment.NewLine}{Environment.NewLine}" +
+                $"Patreon icon used according to published brand guidelines{Environment.NewLine}" +
+                $"https://www.patreon.com/brand{Environment.NewLine}{Environment.NewLine}" +
+                $"GitHub mark icon used according to published logos and usage guidelines{Environment.NewLine}" +
+                $"https://github.com/logos{Environment.NewLine}{Environment.NewLine}" +
+                $"DonationCoder icon used with permission{Environment.NewLine}" +
+                $"https://www.donationcoder.com/forum/index.php?topic=48718{Environment.NewLine}{Environment.NewLine}" +
+                $"PublicDomain icon is based on the following source images:{Environment.NewLine}{Environment.NewLine}" +
+                $"Bitcoin by GDJ - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/vectors/bitcoin-digital-currency-4130319/{Environment.NewLine}{Environment.NewLine}" +
+                $"Letter P by ArtsyBee - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/illustrations/p-glamour-gold-lights-2790632/{Environment.NewLine}{Environment.NewLine}" +
+                $"Letter D by ArtsyBee - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/illustrations/d-glamour-gold-lights-2790573/{Environment.NewLine}{Environment.NewLine}";
+
+            // Set title
+            string programTitle = typeof(MainForm).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
+
+            // Set version for generating semantic version 
+            Version version = typeof(MainForm).GetTypeInfo().Assembly.GetName().Version;
+
+            // Set about form
+            var aboutForm = new AboutForm(
+                $"About {programTitle}",
+                $"{programTitle} {version.Major}.{version.Minor}.{version.Build}",
+                $"Made for: Pbx01, justW3{Environment.NewLine}DonationCoder.com{Environment.NewLine}Day #TBD, Week #TBD @ July 2020",
+                licenseText,
+                this.Icon.ToBitmap());
+
+            // Set about form icon
+            aboutForm.Icon = this.associatedIcon;
+
+            // Show about form
+            aboutForm.ShowDialog();
         }
     }
 }
